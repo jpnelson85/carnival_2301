723919d4f04e15f07bebf6ec16b7f08c5c59d7c6
Jeff Nelson <jpnelson85@gmail.com>
2023-02-27 10:25:33 -0600

    add to iteration 1

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,14 @@
 class Visitor
-  
+  attr_reader :name,
+              :height,
+              :spending_money
+
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money.delete("$").to_i
+    @preferences = []
+  end
+
+  def add_preference
 end
---
+++ spec/visitor_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require './lib/visitor'
+
+RSpec.describe Visitor do
+  it 'exists' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    expect(visitor1).to be_a(Visitor)
+  end
+
+  it 'has attributes' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    expect(visitor1.name).to eq('Bruce')
+    expect(visitor1.height).to eq(54)
+    expect(visitor1.spending_money).to eq(10)
+    expect(visitor1.preferences).to eq([])
+  end
+
+  it 'add preferences' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor1.add_preference
+    expect(visitor1.name).to eq('Bruce')
+
+
+
+  end
+end
