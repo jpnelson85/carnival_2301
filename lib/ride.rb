7b00d3184082921a336bdd3e485b03c940cd63dd
Jeff Nelson <jpnelson85@gmail.com>
2023-02-27 11:12:52 -0600

    iteration 2

--- lib/ride.rb
+++ lib/ride.rb
@@ -9,8 +9,13 @@ class Ride
   @name = ride_details[:name]
   @min_height = ride_details[:min_height]
   @admission_fee = ride_details[:admission_fee]
-  @excitement = ride_details[:gentle]
+  @excitement = :gentle
   @rider_log = {}
-
  end
+
+ def total_revenue
+    0
+ end
+
+
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -15,4 +15,11 @@ RSpec.describe Ride do
     expect(ride1.admission_fee).to eq(1)
     expect(ride1.excitement).to eq(:gentle)
   end
+
+  it 'total revenue is 0' do
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    expect(ride1.total_revenue).to eq(0)
+  end
+
+  
 end
